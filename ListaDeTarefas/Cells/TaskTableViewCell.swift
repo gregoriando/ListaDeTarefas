9adebdd9308deacf265fd92d466243481415f811
Gregory Freire <gregorystp@gmail.com>
2024-05-22 18:01:32 -0300

    Projeto inicial Criado
    Comentário inicial da criação da UITableView, UITableCell e retirada do StoryBoard do propjeto

---
+++ ListaDeTarefas/Cells/TaskTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  TaskTableViewCell.swift
+//  ListaDeTarefas
+//
+//  Created by Gregory Luiz lopes freire on 22/05/24.
+//
+
+import UIKit
+
+class TaskTableViewCell: UITableViewCell {
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        backgroundColor = .black
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ ListaDeTarefas/ViewControllers/TaskTableViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ViewController.swift
+//  ListaDeTarefas
+//
+//  Created by Gregory Luiz lopes freire on 22/05/24.
+//
+
+import UIKit
+
+class TaskTableViewController: UIViewController {
+    
+    
+    private lazy var taskTableView: UITableView = {
+        let tableview = UITableView(frame: .zero)
+        tableview.translatesAutoresizingMaskIntoConstraints = false
+        tableview.backgroundColor = .white
+        tableview.dataSource = self
+        tableview.delegate = self
+        tableview.register(TaskTableViewCell.self, forCellReuseIdentifier: "TaskTableViewCell")
+        return tableview
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        title = "Lista De Tarefas"
+        addSubviews()
+        setupConstraints()
+    }
+    
+    private func addSubviews(){
+        view.addSubview(taskTableView)
+    }
+    
+    private func setupConstraints(){
+        NSLayoutConstraint.activate([
+            taskTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            taskTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            taskTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            taskTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+        
+    }
+    
+
+
+}
+
+extension TaskTableViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 15
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "TaskTableViewCell", for: indexPath) as? TaskTableViewCell {
+            cell.selectionStyle = .none
+            return cell
+        }
+        
+        return UITableViewCell()
+    }
+    
+    
+}
+
+extension TaskTableViewController: UITableViewDelegate {
+
+}
+
